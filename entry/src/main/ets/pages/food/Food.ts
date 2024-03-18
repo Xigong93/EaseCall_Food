0173aad6a17fd35c485c6d1bfa6e23fc9bd8ef60
Xigong <xigong93@qq.com>
2024-03-18 21:15:47 +0800

    添加了我的订单页面和商品列表页面

--- entry/src/main/ets/pages/food/Food.ts
+++ entry/src/main/ets/pages/food/Food.ts
@@ -1,14 +1,8 @@
-export default interface Food {
-  /**
-   * 名称
-   */
+export default class  Food {
+  /** 名称 */
   readonly name: string
-  /**
-   * 图片
-   */
+  /** 图片 */
   readonly image: string
-  /**
-   * 价格，单位元
-   */
+  /** 价格，单位元 */
   readonly price: number
 }
